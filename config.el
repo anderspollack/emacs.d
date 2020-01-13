d3c3c179c76a77577a9edd1b8ead7dc0dd2aae80
anderspollack <pollack.anders@gmail.com>
2020-01-13 12:48:22 -0800

    remove more customizations

--- config.el
+++ config.el
@@ -3,20 +3,6 @@
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
 
-;; (windmove-default-keybindings 'shift)
-;; (global-set-key (kbd "C-M-h") 'windmove-left)
-;; (global-set-key (kbd "C-j") nil)
-;; (global-set-key (kbd "C-M-j") 'windmove-down)
-;; (global-set-key (kbd "C-M-k") 'windmove-up)
-;; (global-set-key (kbd "C-M-l") 'windmove-right)
-
-(global-set-key (kbd "M-n") 'make-frame)
-(global-set-key (kbd "M-w") (lambda () (interactive) (delete-window)))
-(global-set-key [(meta W)] 'delete-frame)
-
-;; (if (not (window-system))
-;;    (xclip-mode t))
-
 (if (not (window-system))
     (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
 (if (not (window-system))
@@ -38,12 +24,12 @@
 ;; PACKAGE INSTALLATION
 ;; package list/update/install - should only be run once or else it slows
 ;; startup time significantly
-(defun install-packages ()
-  "run M-x package-install-selected-packages"
-  (interactive)
-  (package-refresh-contents)
-  (package-install-selected-packages))
-(install-packages)
+;; (defun install-packages ()
+;;   "run M-x package-install-selected-packages"
+;;   (interactive)
+;;   (package-refresh-contents)
+;;   (package-install-selected-packages))
+;; (install-packages)
 
 ;; suppress "ls does not support --dired"
 (when (string= system-type "darwin")       
@@ -63,9 +49,6 @@
 ;; change default "Find File" directory
 (setq default-directory "/Users/anders/")
 
-;; press ESC twice to get out of minibuffer on Find File/Buffer, M-x, etc.
-(global-set-key (kbd "ESC <escape>") 'keyboard-escape-quit)
-
 ;; make mac title bar transparent
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
@@ -77,12 +60,6 @@
 
 ;; alias y/n to yes/no for prompts
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;; resize windows with M-S-[arrow key]
-(global-set-key [(meta shift right)] 'enlarge-window-horizontally)
-(global-set-key [(meta shift left)] 'shrink-window-horizontally)
-(global-set-key [(meta shift down)] 'enlarge-window)
-(global-set-key [(meta shift up)] 'shrink-window)
 
 ;; enable variable pitch for mixed font styles
 (add-hook 'text-mode-hook
@@ -97,12 +74,8 @@
 ;; remember these modes:
 ;; artist-mode, snake. look into org-babel
 
-;;
-;;
-;;
+;; 
 ;; PACKAGE-SPECIFIC CUSTOMIZATIONS
-;;
-;;
 ;;
 
 ;; setup tidal
@@ -112,7 +85,6 @@
 
 ;; projectile mode
 (projectile-mode +1)
-(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; web mode
@@ -171,97 +143,6 @@
 
 ;; setup magit status
 (global-set-key (kbd "C-x g") 'magit-status)
-
-;; enable evil-leader
-(global-evil-leader-mode)
-;; set leader-key to SPACE
-(evil-leader/set-leader "<SPC>")
-;; setup evil-leader keybindings
-(evil-leader/set-key
-  "f" 'find-file
-  "b" 'switch-to-buffer
-  "s" 'save-buffer
-  "=" 'balance-windows
-  "g" 'magit-status
-  "r" 'web-mode
-  "i" 'indent-region
-  "h" 'evil-window-left
-  "j" 'evil-window-down
-  "k" 'evil-window-up
-  "l" 'evil-window-right
-  "1" 'multi-term-dedicated-close-with-other-windows
-  "2" 'evil-window-split
-  "3" 'evil-window-vsplit
-  "0" 'maximize-window
-  "w" 'delete-window
-  "t" 'mult-term-dedicated-open-select 
-  "e" 'web-mode-set-engine
-  "x" 'execute-extended-command
-  "!" 'shell-command
-  "%" 'query-replace
-  "p" 'projectile-command-map
-  "c" 'tidal-run-line
-  "m" 'menu-bar-open
-  )
-
-(defun delete-all-other-windows ()
-  "deletes all other windows including multi-term-dedicated"
-  (interactive)
-  (delete-other-windows)
-  (if (multi-term-dedicated-exist-p)
-      (multi-term-dedicated-close)))
-
-(defun multi-term-dedicated-close-with-other-windows ()
-  "deletes all other winows including multi-term dedicated, or close only multi-term-dedicated if it is the current window"
-  (interactive)
-  (if (multi-term-dedicated-window-p)
-      (multi-term-dedicated-close)
-    (delete-all-other-windows)))
-
-(defun mult-term-dedicated-open-select ()
-  "opens dedicated multi term and switches to it"
-  (interactive)
-  (if (multi-term-dedicated-exist-p)
-      (multi-term-dedicated-close))
-  (multi-term-dedicated-open)
-  (multi-term-dedicated-select)
-  (evil-insert-state)
-  )
-
-;; enable term-paste with C-y
-;; (global-set-key (kbd "C-y") 'term-paste)
-
-;; Enable evil mode
-(require 'evil)
-;; (evil-mode 1)
-
-;; evil-mode page up/down keybindings
-(global-set-key (kbd "M-j") nil)
-(global-set-key (kbd "M-j") 'evil-scroll-page-down)
-(global-set-key (kbd "M-k") nil)
-(global-set-key (kbd "M-k") 'evil-scroll-page-up)
-
-;; j/k for browsing wrapped lines
-(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-
-;; setup evil mode exceptions
-(loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
-                              (comint-mode . emacs)
-                              (shell-mode . emacs)
-                              (term-mode . emacs)
-                              (help-mode . emacs)
-                              (dired-mode . emacs))
-      do (evil-set-initial-state mode state))
-
-;; enable fine undo
-(setq evil-want-fine-undo t)
-
-;; enable evil-surround mode
-(global-evil-surround-mode 1)
-
-(require 'multi-term)
-(setq multi-term-program "/bin/bash")
 
 ;; yasnippet
 ;; (require 'yasnippet)
--- custom.el
+++ custom.el
@@ -10,6 +10,9 @@
     ("26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" default)))
  '(desktop-save-mode t)
  '(electric-pair-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(next-screen-context-lines 5)
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
@@ -21,6 +24,7 @@
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
+ '(winner-mode t)
  '(xterm-mouse-mode t))
 (package-initialize)
 (custom-set-faces
@@ -28,4 +32,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "SF Mono" :foundry "nil" :slant normal :weight normal :height 130 :width normal))))
  '(org-block ((t (:background "#FFFFE0" :foreground "#000088" :family "Iosevka SS02")))))
