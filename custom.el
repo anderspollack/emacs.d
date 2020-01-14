37bbc7de24f762887688e29ac340eb5f6dced39f
anderspollack <pollack.anders@gmail.com>
2020-01-13 17:59:23 -0800

    adds typescript to web mode auto-mode, adds notes

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
     ("26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" default)))
  '(desktop-save-mode t)
  '(electric-pair-mode t)
+ '(global-subword-mode t)
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
  '(next-screen-context-lines 5)
@@ -22,6 +23,7 @@
     (company-lsp company yasnippet lsp-mode xclip yaml-mode multi-term magit evil-leader evil-surround evil-visual-mark-mode emmet-mode web-mode projectile haskell-mode)))
  '(ring-bell-function (quote ignore))
  '(scroll-bar-mode nil)
+ '(sentence-end-double-space nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(winner-mode t)
@@ -32,5 +34,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "SF Mono" :foundry "nil" :slant normal :weight normal :height 130 :width normal))))
+ '(default ((t (:family "Iosevka SS02" :foundry "nil" :slant normal :weight normal :height 130 :width normal))))
  '(org-block ((t (:background "#FFFFE0" :foreground "#000088" :family "Iosevka SS02")))))
